2ebc6f45f6295153e39223c6d42843e1cf331e63
hdvalen <hodethcaballero@gmail.com>
2025-04-29 20:08:53 -0500

    feat: :zap: juego

--- Program.cs
+++ Program.cs
@@ -39,6 +39,50 @@
             Console.WriteLine(nombre);
 
         //Cada producto tiene varias etiquetas
+         Console.WriteLine("\n Productos con sus respectivas etiquetas: ");
+         var etiquetasPorProducto = from p in productos
+                                   join e in productosConEtiquetas  //'join' (unión) entre la lista de productos y la lista de productosConEtiquetas
+                                   on p.Nombre equals e.Producto
+                                   select new { p.Nombre, Etiquetas = string.Join(", ", e.Etiquetas) }; //Unir las etiquetas separadas por comas
+
+        foreach (var item in etiquetasPorProducto)
+            Console.WriteLine($"{item.Nombre} - Etiquetas: {item.Etiquetas}");
         
+
+        //El programa aplanara todas las etiquetas usando SelectMany
+        Console.WriteLine("\nTodas las etiquetas únicas:");
+         var todasLasEtiquetas = productosConEtiquetas
+            .SelectMany(p => p.Etiquetas) // devuelve todas las etiquetas en una sola lista
+            .Distinct();                  // elimina las etiquetas duplicadas
+        foreach (var etiqueta in todasLasEtiquetas)
+        {
+            Console.WriteLine(etiqueta);
+        }
+
+        //El sistema elige aleatoriamente una etiqueta oculta
+
+        var listaEtiquetas = todasLasEtiquetas.ToList();
+        Random random = new Random();
+        string etiquetaOculta = listaEtiquetas[random.Next(listaEtiquetas.Count)];
+
+        //El usuario tiene que adivinar la etiqueta
+
+        Console.WriteLine("\n Adivina la etiqueta secreta");
+        string intentoUsuario;
+        do
+        {
+            Console.Write("Tu intento: ");
+            intentoUsuario = Console.ReadLine();
+
+        if (intentoUsuario.Equals(etiquetaOculta, StringComparison.OrdinalIgnoreCase))  // .Equals() compara los textos y StringComparison.OrdinalIgnoreCase para mayúsculas o minúsculas
+        {
+        Console.WriteLine("🎉 Adivinaste la etiqueta secreta.");
+        }
+         else
+        {   
+        Console.WriteLine($"❌ Incorrecto. Pista: comienza con '{etiquetaOculta[0]}' y tiene {etiquetaOculta.Length} letras.");
+        }
+
+        } while (!intentoUsuario.Equals(etiquetaOculta, StringComparison.OrdinalIgnoreCase));
     }
 }
--- obj/Debug/net8.0/etiquetaOculta.AssemblyInfo.cs
+++ obj/Debug/net8.0/etiquetaOculta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("etiquetaOculta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb18b60d211cf11dcb9f10f5f1a3edc6ba4d360")]
 [assembly: System.Reflection.AssemblyProductAttribute("etiquetaOculta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("etiquetaOculta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
